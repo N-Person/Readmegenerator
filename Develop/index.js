f682498602195ae76c758cee6f741e1e223bc5f6
Brian Morgan <brianjm2007@yahoo.com>
2021-05-22 15:19:00 -0400

    trying to commit for the 5th time, killed server, fixed gitignore

--- Develop/index.js
+++ Develop/index.js
@@ -1,83 +1,85 @@
-// TODO: Include packages needed for this application
+// These are the dependencies for NODE JS for the generator to run.
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown.js");
-const Choices = require("inquirer/lib/objects/choices");
 
-
-// TODO: Create an array of questions for user input
+// Array of questions for user.
 const questions = [
-    {
-        type: "input"
-        message: "What is the name of your project?"
-        name: "title"
-    },
-
-    {
-        type: "input"
-        message: "What is the name of your project?"
-        name: "description"
-    },
-    {
-        type: "input"
-        message:"Please provide usage instructions"
-        name: "contribution"
-    },
-
-    {
-        type: "input"
-        message:"Please explain how to test this code."
-        name: "test"
-    },
-    {
-        type: "input"
-        message: "What is your preferred license of this project?"
-        name: "license"
-        choices: [
-            {
-                name: "MIT Lincense",
-              },
-              {
-                name: "Apache License 2.0",
-              },
-              {
-                name: "GNU General Public License",
-              },
-              {
-                name: "Mozilla Public License 2.0",
-              },  
-        ],
-    },
-    {
-        type: "input"
-        message:"What is your Email?"
-        name: "email"
-    },
-    {
-        type: "input"
-        message:"What is your GitHub username?"
-        name: "username"
-    },
+  {
+    type: "input",
+    message: "What is the name of your project?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Please provide a description of your project.",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "Please provide installation instructions.",
+    name: "install",
+  },
+  {
+    type: "input",
+    message: "Please provide usage instructions.",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "Please list any contribution instructions.",
+    name: "contribution",
+  },
+  {
+    type: "input",
+    message: "Please explain how to test this code.",
+    name: "test",
+  },
+  {
+    type: "list",
+    message: "What is your preferred license of this project?",
+    name: "license",
+    choices: [
+      {
+        name: "MIT Lincense",
+      },
+      {
+        name: "Apache License 2.0",
+      },
+      {
+        name: "GNU General Public License",
+      },
+      {
+        name: "Mozilla Public License 2.0",
+      },
+    ],
+  },
+  {
+    type: "input",
+    message: "What is your email?",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "username",
+  },
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
+// This is the function that using the generateMarkdown and the user's questions to generate the complete README.
 function init() {
-    inquirer.prompt(questions).then((data)=> {
-        fs.writeFile('./outputReadme/README.md', generateMarkdown(data), function (
-            err
-        ) {
-            if (err) {
-                return console.log(err);
-            }
+  inquirer.prompt(questions).then((data) => {
+    fs.writeFile("./outputReadme/README.md", generateMarkdown(data), function (
+      err
+    ) {
+      if (err) {
+        return console.log(err);
+      }
 
-            console.log('Success!')
-            });
-        });
-    }
+      console.log("Success!");
+    });
+  });
+}
 
-
-// Function call to initialize app
+// function call to initialize program
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,34 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
+// function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+<img src="https://img.shields.io/badge/license-${data.license}-red"/>
+  
+## Table of Contents
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Lincense](#license) 
+* [Contributing](#contributing)
+* [Test](#test)
+* [Questions](#questions)  
+     
+## Description
+${data.description}
+## Installation Instructions
+${data.install}
+## Usage
+${data.usage}
+## License
+This project is being licensed under the ${data.license} license.
+## Contributing
+${data.contribution}
+## Test
+${data.test}
+## Questions
+If you have any questions, please contact ${data.email} or visit https://github.com/${data.username}
 `;
 }
 
 module.exports = generateMarkdown;
+// This allows us to export the generateMarkdown.js into the index.js
